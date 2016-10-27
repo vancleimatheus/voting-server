9b9946fa0a1824dc8119d392fc7cb0b332f4242d
Vanclei Matheus <vmatheus@vfgconsulting.com>
2016-10-26 23:32:07 -0200

    Implemented reducer

--- src/core.js
+++ src/core.js
@@ -1,16 +1,34 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
 }
 
-export function next(state) {
-    const entries = state.get('entries');
+function getWinners(vote) {
+    if(!vote) return [];
+    const [a,b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+    if (aVotes > bVotes) return [a];
+    else if (aVotes < bVotes) return [b];
+    else return [a,b];
+}
 
-    return state.merge({
-        vote: Map({pair: entries.take(2)}),
-        entries: entries.skip(2)
-    });
+export function next(state) {
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
+
+    console.log(entries.size);
+    
+    if(entries.size === 1) {
+        return state.remove('vote').remove('entries').set('winner', entries.first());       
+    } else {
+        return state.merge({
+            vote: Map({pair: entries.take(2)}),
+            entries: entries.skip(2)
+        });
+    }
 }
 
 export function vote(state, entry) {
